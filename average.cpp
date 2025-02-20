b5cac8e15f68aca3ffb9731969efb5c6b1a0918e
NNLLP <98796613+NNLLP@users.noreply.github.com>
2025-02-21 00:57:04 +0700

    average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    vector<double> numbers;
+    for (int i = 1; i < argc; i++) {
+        numbers.push_back(atof(argv[i]));
+    }
+
+    double sum = 0;
+    for (double num : numbers) {
+        sum += num;
+    }
+
+    double average = sum / numbers.size();
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << numbers.size() << " numbers = " << fixed << setprecision(3) << average << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
 
